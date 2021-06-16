fc6759bacdb75191246d15b861862e68d2e9e5f4
Florent Ascensio <flo654@hotmail.com>
2021-06-16 14:06:46 +0200

    fix bug css

--- public/index.php
+++ public/index.php
@@ -8,15 +8,14 @@ use App\controllers\Render;
 use App\controllers\Router;
 use App\controllers\User;
 
-$whoops = new \Whoops\Run;
+/* $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
+ */
 
-
+ 
 session_start();
-//var_dump($_SERVER);exit;
 
-//$router = new Router;
 
 $render = new Render;
 
@@ -124,36 +123,7 @@ try {
         return;
     }
     throw new Exception("Not Found !!", 404);
-    
-    /* ////////////////////////////////////////////////////////
-    /////////////////////// ROUTES//////////////////////////
-    ////////////////////////////////////////////////////////
-
-    $router->post('signIn', 'loginController');
-    $router->post('signUp', 'createUserController');
-    $router->post('logout', 'createUserController');
-    $router->post('createArticle', 'CreateArticleController');
-    $router->post('createComment', 'CreateCommentController');
-    $router->post('modifyArticle', 'modifyCommentController');//modify
-    $router->post('validateComment', 'validateComment');//modify
-    $router->post('deleteComment', 'deleteComment');//delete
-
-    ////////////////FRONT OFFICE ROUTE//////////////////////
-
-    $router->get('/home', 'homeController');
-    $router->get('/blog', 'postListController');
-    $router->get('/blog/article-:id', 'singlePostController');
-    $router->get('/contact', 'contactController');
-
-    ////////////////BACK OFFICE ROUTE///////////////////////
-    ////////////////////////////////////////////////////////
-
-    $router->get('/admin', 'adminController');
-    $router->get('/admin/article/create', 'articleController');    
-    $router->get('/admin/comments', 'commentsController');
-    $router->get('/admin/postList', 'adminPostList');
-    $router->get('/backArticle-:id', 'modifyController');//modify
-    $router->get('/deleteArticle-:id', 'deleteController');//delete */
+   
 
 } catch (Exception $e) {
 
